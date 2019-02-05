be64538a697c365a699a20900e02fab69592517c
ershad <ershad@gmail.com>
2019-02-05 17:02:29 +0600

    Polishing

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/bean/HelloWorldBean.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/bean/HelloWorldBean.java
@@ -14,4 +14,11 @@ public class HelloWorldBean {
   public void setMessage(String message) {
     this.message = message;
   }
+
+  @Override
+  public String toString() {
+    return "HelloWorldBean{" +
+      "message='" + message + '\'' +
+      '}';
+  }
 }
